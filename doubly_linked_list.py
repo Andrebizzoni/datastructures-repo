25560d0a30df93d0fba6df6948657ba8b203c39e
Andrea Bizzoni <andreabizzoni@andres-mbp.home>
2022-02-25 11:58:53 +0000

    added doubly linked list file and improved code for linked list file

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -4,11 +4,12 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
-class LinkedListIterator:
+class DoublyLinkedListIterator:
     def __init__(self, head):
         self.current = head
-
+    
     def __iter__(self):
         return self
     
@@ -20,44 +21,51 @@ class LinkedListIterator:
             self.current = self.current.next
             return item
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-
+    
     def __iter__(self):
-        return LinkedListIterator(self.head)
+        return DoublyLinkedListIterator(self.head)
     
     def addFront(self, data):
         new_node = Node(data)
         if not self.head:
             self.head = new_node
             return
-        new_node.next = self.head
-        self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
     
     def addEnd(self, data):
         new_node = Node(data)
         if not self.head:
             self.head = new_node
             return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
-
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = new_node
+            new_node.prev = last
+    
     def addAfter(self, prev_node_data, data):
         new_node = Node(data)
         current = self.head
         while current.next:
             if current.data == prev_node_data:
-                new_node.next = current.next
-                current.next = new_node
-                return
+               new_node.next = current.next
+               current.next.prev = new_node
+               current.next = new_node
+               new_node.prev = current
+               return
             else:
                 current = current.next
-        print("Item is not in LinkedList")
+        return print("Item is not in DoublyLinkedList")
 
     def popFront(self):
+        self.head.next.prev = None
         temp = self.head.next
         self.head.next = None
         self.head = temp
@@ -67,27 +75,35 @@ class LinkedList:
         penultimate = self.head
         while penultimate.next.next:
             penultimate = penultimate.next
+        penultimate.next.prev = None
         penultimate.next = None
         gc.collect()
-    
+
     def popNode(self, node_data):
         current = self.head
         if current.data == node_data:
             self.popFront()
             return
         while current.next:
-            if current.next.data == node_data:
-                current.next = current.next.next
-                gc.collect()
+            if current.data == node_data:
+                previous = current.prev
+                previous.next = current.next
+                current.next.prev = previous
+                current.prev = None
+                current.next = None
+                gc.collect
                 return
             else:
                 current = current.next
-        print("Item is not in LinkedList")
+        if current.data == node_data:
+            self.popEnd()
+            return
+        return print("Item is not in DoublyLinkedList")
 
     def sort(self, reverse=False):
         while True:
-            current = self.head
             no_swaps = True
+            current = self.head
             while current.next:
                 if reverse:
                     if current.data < current.next.data:
@@ -100,16 +116,26 @@ class LinkedList:
             if no_swaps:
                 return
 
-    def max(self):
-        max = self.head.data
-        for item in self:
-            if item > max:
-                max = item    
-        return max
-
     def min(self):
         min = self.head.data
         for item in self:
             if item < min:
                 min = item
         return min
+
+    def max(self):
+        max = self.head.data
+        for item in self:
+            if item > max:
+                max = item
+        return max
+
+list = DoublyLinkedList()
+list.addFront(5)
+list.addFront(10)
+list.addEnd(9)
+list.addEnd(4)
+list.addAfter(9, 6)
+list.sort(reverse=True)
+for item in list:
+    print(item)
